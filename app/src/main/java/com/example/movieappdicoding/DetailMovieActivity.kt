951c92ae554e97a13340c1fc4754355020aba94b
Mattjevaas <johaneswiku@protonmail.ch>
2023-08-01 15:44:36 +0700

    feat: add share feature

--- app/src/main/java/com/example/movieappdicoding/DetailMovieActivity.kt
+++ app/src/main/java/com/example/movieappdicoding/DetailMovieActivity.kt
@@ -1,9 +1,13 @@
 package com.example.movieappdicoding
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.bumptech.glide.Glide
 import com.example.movieappdicoding.databinding.ActivityDetailMovieBinding
 
@@ -14,6 +18,7 @@ class DetailMovieActivity : AppCompatActivity() {
     }
 
     private lateinit var binding: ActivityDetailMovieBinding
+    private var movieData: Movie? = null
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +29,7 @@ class DetailMovieActivity : AppCompatActivity() {
         supportActionBar?.title = "Movie Detail"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val movieData = if (Build.VERSION.SDK_INT >= 33) {
+        movieData = if (Build.VERSION.SDK_INT >= 33) {
             intent.getParcelableExtra(EXTRA_MOVIE, Movie::class.java)
         } else {
             @Suppress("DEPRECATION")
@@ -33,17 +38,39 @@ class DetailMovieActivity : AppCompatActivity() {
 
         if(movieData != null) {
             Glide.with(binding.root.context)
-                .load(movieData.images.first())
+                .load(movieData!!.images.first())
                 .centerCrop()
                 .placeholder(R.drawable.img_placeholder_svg)
                 .into(binding.imageViewMovie)
 
-            binding.textTitle.text = movieData.title
-            binding.textReleased.text = "Release On: ${movieData.released}"
-            binding.textDirector.text = "Director: ${movieData.director}"
-            binding.textGenre.text = "Genre: ${movieData.genre}"
-            binding.textPlot.text = movieData.plot
+            binding.textTitle.text = movieData!!.title
+            binding.textReleased.text = "Release On: ${movieData!!.released}"
+            binding.textDirector.text = "Director: ${movieData!!.director}"
+            binding.textGenre.text = "Genre: ${movieData!!.genre}"
+            binding.textPlot.text = movieData!!.plot
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.detail_bar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_share -> {
+                val sendIntent: Intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, "${movieData!!.title} - ${movieData!!.plot}")
+                    type = "text/plain"
+                }
+
+                val shareIntent = Intent.createChooser(sendIntent, null)
+                startActivity(shareIntent)
+            }
+        }
+
+        return false
     }
 
     override fun onSupportNavigateUp(): Boolean {
